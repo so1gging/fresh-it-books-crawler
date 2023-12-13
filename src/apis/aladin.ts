c9c76a234ede3dbf248235f53034e65d06dbb58d
김솔지 <so1gging@gmail.com>
2023-12-13 16:54:28 +0900

    :necktie: aladin yes24에서 크롤링 해오는 로직 분류

---
+++ src/apis/aladin.ts
@@ -0,0 +1,38 @@
+import { getCherrioAPI, getHtml } from '../utils/functions'
+import { GetBookInfo } from '../utils/types'
+import fs from 'fs'
+
+const URL = 'https://www.aladin.co.kr/shop/common/wnew.aspx?NewType=SpecialNew&BranchType=1&CID=351'
+
+export default async function getAladinList() {
+  let page = 1
+  const list = new Array<GetBookInfo>()
+
+  while (true) {
+    const html = await getHtml(URL + `&page=${page}`)
+    const $ = getCherrioAPI(html?.data)
+
+    const $booklist = $('form#Myform').children('div.ss_book_box')
+
+    if ($booklist.length === 0) {
+      break
+    }
+
+    $booklist.each((i, elem) => {
+      const subjects = $(elem).find('.ss_book_list').first().find('ul>li>a.bo3').text()
+      const image = $(elem).find('.flipcover_in>img').attr('src')
+      list.push({
+        name: subjects,
+        imageUrl: image,
+      })
+    })
+
+    page += 1
+  }
+
+  const jsonString = JSON.stringify(list)
+  fs.writeFile('src/data/aladin.json', jsonString, (err) => {
+    if (err) return console.log('❌ ERROR :', err)
+    console.log(`📌 총 ${list.length}건의 알라딘 신간 정보를 저장했어요 !`)
+  })
+}
---
+++ src/apis/yes24.ts
@@ -0,0 +1,37 @@
+import { getCherrioAPI, getHtml } from '../utils/functions'
+import * as cheerio from 'cheerio'
+import { GetBookInfo } from '../utils/types'
+import fs from 'fs'
+const URL = 'https://www.yes24.com/24/Category/NewProductList/001001003020?sumGb=04'
+
+export default async function getYes24() {
+  let page = 1
+  const list = new Array<GetBookInfo>()
+
+  while (true) {
+    const html = await getHtml(URL + `&PageNumber=${page}`)
+    const $ = getCherrioAPI(html?.data, 'euc-kr')
+    const $booklist = $('table#category_layout>tbody').children('tr')
+
+    if ($booklist.length === 0) {
+      break
+    }
+
+    $booklist.each((i, elem) => {
+      const subjects = $(elem).find('.goodsTxtInfo>p').text()
+      const image = $(elem).find('.goodsImgW>a').first().find('img').attr('src')
+      list.push({
+        name: subjects,
+        imageUrl: image,
+      })
+    })
+
+    page += 1
+  }
+
+  const jsonString = JSON.stringify(list)
+  fs.writeFile('src/data/yes24.json', jsonString, (err) => {
+    if (err) return console.log('❌ ERROR :', err)
+    console.log(`📌 총 ${list.length}건의 yes24 신간 정보를 저장했어요 !`)
+  })
+}
